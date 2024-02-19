b68d66a0b961888cc13d37e5df1028840c6fc824
DevLaukey <devlaukeymwaura@gmail.com>
2024-02-20 00:49:12 +0300

    added auth to the backend

---
+++ .history/farm_management_dashboard/farm_management_app/views_20240220004036.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+from .models import Crop, Employee, Livestock, Machinery
+from .serializers import CropSerializer, EmployeeSerializer, LivestockSerializer, MachinerySerializer
+from dj_rest_auth.registration.views import RegisterView
+
+class CropViewSet(viewsets.ModelViewSet):
+    queryset = Crop.objects.all()
+    serializer_class = CropSerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class LivestockViewSet(viewsets.ModelViewSet):
+    queryset = Livestock.objects.all()
+    serializer_class = LivestockSerializer
+
+class MachineryViewSet(viewsets.ModelViewSet):
+    queryset = Machinery.objects.all()
+    serializer_class = MachinerySerializer
+
+class CustomRegisterView(RegisterView):
+    serializer_class = CustomRegisterSerializer
