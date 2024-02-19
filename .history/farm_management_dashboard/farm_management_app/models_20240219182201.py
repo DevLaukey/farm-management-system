7f40a2b91688d6020deba8ae977d70b35fbd853a
DevLaukey <devlaukeymwaura@gmail.com>
2024-02-19 18:56:00 +0300

    database complete

--- .history/farm_management_dashboard/farm_management_app/models_20240219182201.py
+++ .history/farm_management_dashboard/farm_management_app/models_20240219182201.py
@@ -1,19 +1,19 @@
+
 from django.db import models
-from datetime import date
 
 class Crop(models.Model):
     name = models.CharField(max_length=255)
     variety = models.CharField(max_length=255)
     planting_date = models.DateField()
     harvest_date = models.DateField()
-
+    
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
     position = models.CharField(max_length=255)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     performance = models.TextField()
-
+    
 class Livestock(models.Model):
     animal_type = models.CharField(max_length=255)
     breed = models.CharField(max_length=255)
@@ -21,15 +21,11 @@ class Livestock(models.Model):
     weight = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_date = models.DateField()
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def calculate_age_in_months(self):
-        today = date.today()
-        purchase_month = (today.year - self.purchase_date.year) * 12 + today.month - self.purchase_date.month
-        return purchase_month
-
+    
 class Machinery(models.Model):
     equipment_name = models.CharField(max_length=255)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_date = models.DateField()
     maintenance_date = models.DateField()
     maintenance_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    
